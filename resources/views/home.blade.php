c72bd5fc9fb6b16f050e7b04e60de06daca0add7
Suzzanne20 <susanacevedo11@hotmail.com>
2023-03-04 08:46:41 -0600

    aynooo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    <p>BIENVENIDO</p>
+@endsection
+
+@section('content')
+    <p>PAGINA 1</p> <a>pag1.blade.php</a>
+    <p>PAGINA 2</p> <a>pag2.blade.php</a>
+@endsection
---
+++ resources/views/pag1.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    <p>Este es el texto de la vista segunda.</p>
+@endsection
+
+@section('content')
+    <p>home.blade.php</p>
+
+@endsection
